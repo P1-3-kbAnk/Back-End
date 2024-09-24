9e9a18d999d87055e178e8a4aab39f0088cd280d
sungheonkim <tjdgjswkd@naver.com>
2024-09-24 10:56:20 +0900

    :sparkles: Feat     : ENUM추가 PROVIDER는 상민이 추가하신대여

--- src/main/java/com/kbank/backend/enumerate/Rate.java
+++ src/main/java/com/kbank/backend/enumerate/Rate.java
@@ -1,4 +1,12 @@
 package com.kbank.backend.enumerate;
 
+/*
+제목 : 복약시기 enum
+설명 : 주석 참고
+담당자 : 김성헌
+*/
+
+//이거 필요없는 ENUM인듯?
+
 public enum Rate {
 }
--- src/main/java/com/kbank/backend/enumerate/Role.java
+++ src/main/java/com/kbank/backend/enumerate/Role.java
@@ -1,4 +1,12 @@
 package com.kbank.backend.enumerate;
 
+
+/*
+제목 : 역할 enum
+설명 : 주석 참고
+담당자 : 김성헌
+*/
 public enum Role {
+    ADMIN,
+    USER
 }
