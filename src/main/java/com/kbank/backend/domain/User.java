3589e1da1954e304e24abf06b74e5b865def5771
Sangmin Han <gkstkdals07@naver.com>
2024-09-24 13:28:17 +0900

    :sparkles: Feat: 유저 도매인 작성

--- src/main/java/com/kbank/backend/domain/User.java
+++ src/main/java/com/kbank/backend/domain/User.java
@@ -1,6 +1,5 @@
 package com.kbank.backend.domain;
 
-
 /*
 제목 : 회원 정보 엔티티 정의
 설명 :
@@ -10,23 +9,37 @@ package com.kbank.backend.domain;
 import com.kbank.backend.enumerate.Gender;
 import com.kbank.backend.enumerate.Provider;
 import com.kbank.backend.enumerate.Role;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.DynamicUpdate;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
 @Table(name="user_tb")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가 설정
-    @Column(name="user_pk")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_pk", nullable = false, updatable = false, unique = true)
     private Long userPk;
+
+    @Column(name="provider", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @Column(name="social_id", nullable = false)
+    private String socialId;
+
+    @Column(name="role", nullable = false)
+    private Role role;
+
+    @Column(name = "create_date", nullable = false)
+    private LocalDateTime createTime;
+
+    /* User Info */
 
     @Column(name="user_nm")
     private String userNm;
@@ -35,38 +48,41 @@ public class User {
     private String phoneNo;
 
     @Column(name="gender")
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @Column(name="first_no")
-    private int firstNo;
+    private String firstNo;
 
     @Column(name="last_no")
-    private int lastNo;
+    private String lastNo;
 
-    @Column(name="provider")
-    private Provider provider;
-
-    @Column(name="social_id")
-    private String socialId;
-
-    @Column(name="role")
-    private Role role;
-
-
-    @Column(name = "bank_nm", nullable = false)
+    @Column(name = "bank_nm")
     private String bankNm;
 
-    @Column(name = "account", nullable = false)
+    @Column(name = "account")
     private long account;
 
-    @Column(name = "account_no", nullable = false)
+    @Column(name = "account_no")
     private String accountNo;
 
-    @Column(name = "account_pw", nullable = false)
+    @Column(name = "account_pw")
     private String accountPw;
 
-    @Column(name = "create_time", nullable = false)
-    private LocalDateTime createTime;
+    /* Relation */
+    // one to many 와 같은 관계 설정
 
+    @Builder
+    public User(Provider provider, String socialId, Role role, LocalDateTime createTime) {
+        this.provider = provider;
+        this.socialId = socialId;
+        this.role = role;
+        this.createTime = createTime;
+    }
 
+    /* Update */
+    
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
