c09522e296e727da61f03d0a4c051c0e9799c49f
sungheonkim <tjdgjswkd@naver.com>
2024-09-25 09:33:08 +0900

    :art: Refactor: 부담률 ENUM 수정

--- src/main/java/com/kbank/backend/enumerate/CopaymentRateCd.java
+++ src/main/java/com/kbank/backend/enumerate/CopaymentRateCd.java
@@ -1,18 +1,25 @@
 package com.kbank.backend.enumerate;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /*
 제목 : 본인부담률 enum
 설명 : 주석 참고
 담당자 : 김성헌
 */
-
+@Getter
+@RequiredArgsConstructor
 public enum CopaymentRateCd {
-    FULL_COVERAGE,      // 전액 본인 부담 (100%)
-    PARTIAL_COVERAGE,   // 부분 부담 (일반적인 본인부담률)
-    LOW_INCOME,         // 저소득층
-    DISABLED,           // 장애인
-    ELDERLY,            // 노인 (65세 이상)
-    PREGNANT,           // 임산부
-    MILITARY,           // 군인
-    EXEMPTION           // 면제 (본인 부담 없음)
+
+    A("A","100분의 50 본인부담"),
+    B("B","100분의 80본인부담" ),
+    C("C","100분의 30본인부담"),
+    D("D","건강보험(의료급여) 100분의 100본인부담"),
+    V("V","보훈 등 100분의 100부담"),
+    W("W","보훈 비급여");
+
+    private final String name;
+    private final String example;
+
 }
