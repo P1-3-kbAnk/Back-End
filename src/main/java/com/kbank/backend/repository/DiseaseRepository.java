ef19c136739b6df65d540c574d3c0001a88c6bb0
doeun <doeun0509@naver.com>
2024-09-26 09:24:41 +0900

    :sparkles: Feat: Disease 엔티티 수정 및 서비스 구현

--- src/main/java/com/kbank/backend/repository/DiseaseRepository.java
+++ src/main/java/com/kbank/backend/repository/DiseaseRepository.java
@@ -8,11 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+import java.util.Optional;
 
 
 @Repository
 public interface DiseaseRepository extends JpaRepository<Disease, Long> {
+
+    Optional<Disease> findById(Long id);
+
+    List<Disease> findAll();
 
     // 특정 Prescription에 대한 질병 조회
     List<Disease> findByDiseasePrescriptionFk(Prescription prescription);
