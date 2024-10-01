ff1295aa0aeafff296ff41db03dc04dd99036d8c
doeun <doeun0509@naver.com>
2024-10-01 14:14:32 +0900

    :art: Refactor: HospitalBill 수정

--- src/main/java/com/kbank/backend/repository/HospitalBillRepository.java
+++ src/main/java/com/kbank/backend/repository/HospitalBillRepository.java
@@ -1,7 +1,6 @@
 package com.kbank.backend.repository;
 
 import com.kbank.backend.domain.HospitalBill;
-import com.kbank.backend.domain.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,14 +13,7 @@ public interface HospitalBillRepository extends JpaRepository<HospitalBill, Long
 
     Optional<HospitalBill> findById(long id);
 
-    List<HospitalBill> findAll();
-
-    // 특정 totalPrice로 Bill 조회
-    List<HospitalBill> findByTotalPrice(long totalPrice);
-
-    // 특정 날짜에 생성된 Bill 조회
-    List<HospitalBill> findByCreateYmd(LocalDateTime CreateYmd);
-
     // 특정 Prescription에 대한 Bill 조회
-    List<HospitalBill> findByHospitalBillPrescription(Prescription prescription);
+    Optional<HospitalBill> findByHospitalBillPrescriptionPrescriptionPk(Long prescriptionId);
+
 }
