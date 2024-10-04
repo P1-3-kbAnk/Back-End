2019144b7032cc992524c6ded27480789b858000
Sangmin Han <gkstkdals07@naver.com>
2024-10-04 15:04:39 +0900

    :sparkles: Feat: 보험 청구 여부 변경 api

--- src/main/java/com/kbank/backend/repository/PrescriptionRepository.java
+++ src/main/java/com/kbank/backend/repository/PrescriptionRepository.java
@@ -3,6 +3,8 @@ package com.kbank.backend.repository;
 
 import com.kbank.backend.domain.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +14,10 @@ import java.util.Optional;
 @Repository
 public interface PrescriptionRepository extends JpaRepository<Prescription,Long>{
     //조제여부 false인 처방전 목록 조회
-    List<Prescription> findByPrescriptionStFalse();
+
+    @Query(value = "SELECT * FROM prescription_tb p WHERE p.pre_user_fk = :userId " +
+            "AND p.prescription_st IS FALSE", nativeQuery = true)
+    List<Prescription> findByUserPkAndPreStFalse(@Param("userId") Long userId);
 
     Optional<Prescription> findByPrescriptionNo(int prescriptionNo);
 
