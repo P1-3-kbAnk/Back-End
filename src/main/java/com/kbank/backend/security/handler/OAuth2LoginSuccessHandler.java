14275d2071c28f5d8597fe99122845def0dae659
Sangmin Han <gkstkdals07@naver.com>
2024-10-07 17:26:15 +0900

    :sparkles: Feat: security handler 추가

---
+++ src/main/java/com/kbank/backend/security/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,44 @@
+package com.kbank.backend.security.handler;
+
+import com.kbank.backend.dto.JwtTokenDto;
+import com.kbank.backend.security.info.UserPrincipal;
+import com.kbank.backend.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+    private final JwtUtil jwtUtil;
+    private static final String REDIRECT_URL = "http://localhost:3000/redirect";
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+    }
+
+    @Override
+    @Transactional
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        JwtTokenDto jwtTokenDto = jwtUtil.generateTokens(userPrincipal.getId(), userPrincipal.getRole());
+
+        String redirectUrl = REDIRECT_URL;
+
+        // 쿼리 문자열을 추가
+        redirectUrl += "?accessToken="+jwtTokenDto.getAccessToken()+"&role="+userPrincipal.getRole();
+
+        response.sendRedirect(redirectUrl);
+    }
+}
